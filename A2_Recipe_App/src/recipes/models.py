44840e3650132d009595886dbc240c7b893c442c
fonzmartinez <119453742+fonzmartinez@users.noreply.github.com>
2023-08-18 22:16:26 -0500

    Added images

--- A2_Recipe_App/src/recipes/models.py
+++ A2_Recipe_App/src/recipes/models.py
@@ -6,7 +6,8 @@ class Recipe(models.Model):
     recipe_name= models.CharField(max_length=120)
     ingredients= models.TextField()
     cooking_time= models.PositiveIntegerField()
-    pic = models.ImageField(upload_to='customers', default='no_picture.jpg')
+    pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
+
     
     def __str__(self):
         return str(self.recipe_name)
