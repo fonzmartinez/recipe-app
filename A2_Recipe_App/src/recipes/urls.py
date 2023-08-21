fd3dac5080af91d2f5790e32804b284f0a52b8f0
fonzmartinez <119453742+fonzmartinez@users.noreply.github.com>
2023-08-21 01:45:53 -0500

    Added List View

--- A2_Recipe_App/src/recipes/urls.py
+++ A2_Recipe_App/src/recipes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import home
+from .views import RecipeListView
 
 app_name = 'recipes'
 
 urlpatterns = [
-   path('', home)
+   path('', home),
+   path('list/', RecipeListView.as_view(), name='list'),
 ]
