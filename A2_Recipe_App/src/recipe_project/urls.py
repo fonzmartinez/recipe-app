c6220d578d1e15f95af5338b28204e7d1b93f643
fonzmartinez <119453742+fonzmartinez@users.noreply.github.com>
2023-08-23 14:11:41 -0500

    Created login feature

--- A2_Recipe_App/src/recipe_project/urls.py
+++ A2_Recipe_App/src/recipe_project/urls.py
@@ -19,11 +19,13 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
-    path('recipes/', include('recipes.urls'))
+    path('recipes/', include('recipes.urls')),
+    path('login/', login_view, name='login')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
